e538ee2aeb727eb9be7b3420db3cb7b62021d0f8
Sai Krishna Dasoju <saidaso@microsoft.com>
2017-12-17 19:10:04 +0530

    changes for handling error in passport

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ var mongoose = require("mongoose");
 var User = require("./models/User");
 var jwt = require("jwt-simple");
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 mongoose.connect("mongodb://test:test@ds137246.mlab.com:37246/mydb")
 var app = express();
 
 app.use(bodyParser.json());
+app.use(passport.initialize());
 
-app.get("/hello", function (req, res) {
-    res.send("this is working");
-});
-
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+})
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -22,23 +25,89 @@ app.use(function (req, res, next) {
 })
 
 
-app.post("/login", function (req, res) {
-    req.user = req.body;
-    var searchUser = { userName: req.user.userName };
+
+var strategyOptions = { usernameField: 'userName' };
+
+var registerStrategyOptions = { usernameField: 'userName', passReqToCallback: true };
+var loginStrategy = new LocalStrategy(strategyOptions, function (userName, password, done) {
+    var searchUser = { userName: userName }
+
     User.findOne(searchUser, function (err, user) {
-        if (err) throw err;
-        if(!user){
-           return res.status(401).send({ message: "wrong UserName/password" });
+        if (err) return done(err);
+        if (!user) {
+            return done(null, false, { message: "wrong UserName/password" });
         }
-        user.comparePasswords(req.user.password, function (err, isValidpassword) {
-            if (err) throw err;
+        user.comparePasswords(password, function (err, isValidpassword) {
+            if (err) return done(err);
             if (!isValidpassword) {
-              return  res.status(401).send({ message: "wrong password" });
+                return done(null, false, { message: "wrong password" });
             }
-            createToken(user,res);
+            return done(null, user);
         });
     })
-})
+
+});
+var registerStrategy = new LocalStrategy(registerStrategyOptions, function (req, name, userName, done) {
+    var user = req.body;
+    var searchUser = { $or: [{ userName: user.userName }, { email: user.email }] }
+
+    User.find(searchUser, function (err, users) {
+        if (err) return done(err);
+        if (users && users.length > 0) {
+            return done(null, false, { message: "username/email already exists" });
+        }
+        var newUser = new User({
+            name: user.name,
+            userName: user.userName,
+            email: user.email,
+            password: user.password,
+        });
+        newUser.save(function (err) {
+            done(null, newUser);
+        })
+    })
+
+
+
+
+});
+
+passport.use('local-login', loginStrategy);
+passport.use('local-register', registerStrategy);
+
+app.post("/login", function (req, res, next) {
+    passport.authenticate('local-login', function (err, user,info) {
+        if (err)
+            next(err);
+        if (!user) {
+            return res.send(400, info);
+
+        }
+        req.login(user, function (err) {
+            if (err)
+                next(err);
+            createToken(user, res);
+        })
+
+    })(req, res, next)
+});
+app.post("/register", function (req, res, next) {
+    passport.authenticate('local-register', function (err, user, info) {
+        if (err)
+            next(err);
+
+        if (!user) {
+            return res.send(400, info);
+
+        }
+        req.login(user, function (err) {
+            if (err)
+                next(err);
+            createToken(user, res);
+        })
+
+    })(req, res, next)
+});
 
 function createToken(user, res) {
     var payload = {
@@ -62,43 +131,16 @@ app.get("/greetings", function (req, res) {
             message: "You are not authorised"
         })
     }
-
     var token = req.headers.authorization.split(' ')[1];
     var payload = jwt.decode(token, "mysecret");
 
     if (!payload.sub) {
         res.status(401).send({ message: "not authorised" })
     }
-
-
-
     res.status(200).send(JSON.stringify({
         greetings: [{}, {}]
     }))
 })
-app.post("/register", function (req, res) {
-
-    var user = req.body;
-    var newUser = new User({
-        name: user.name,
-        userName: user.userName,
-        email: user.email,
-        password: user.password,
-    });
-
-    var payload = {
-        issuer: req.hostname,
-        sub: newUser._id
-    }
-
-    var jwtoken = jwt.encode(payload, "mysecret");
-    newUser.save(function (err) {
-
-        createToken(newUser, res);
-    })
-
-
-});
 
 
 
